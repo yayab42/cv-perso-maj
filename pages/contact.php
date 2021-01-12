5d69ffd02fb50f0bd8f05fa3ff76365a1d34f3dc
yayab42 <yannis.bounia@outlook.com>
2021-01-12 16:29:50 +0100

    Etape 4.3 FIN --> les champs vides sont vérifiés, un message d'error personnalisé apparaît pour chaque champ.

--- pages/contact.php
+++ pages/contact.php
@@ -1,78 +1,119 @@
 <?php
-$page='contact';
-$metaTitle ="Page contact CV";
+$page = 'contact';
+$metaTitle = "Page contact CV";
 
-$dateactuelle=date('Y-m-d-H-i-s');
-$raison=filter_input(INPUT_POST, 'Raison');
-$civilite=filter_input(INPUT_POST, 'civilite');
-$Nom=filter_input(INPUT_POST, 'Nom');
-$Prenom=filter_input(INPUT_POST, 'Prenom');
-$mail=filter_input(INPUT_POST, 'mail');
-$Message=filter_input(INPUT_POST, 'Message');
-file_put_contents("contact_$dateactuelle.txt", "Raison du contact : $raison \n", FILE_APPEND|LOCK_EX);
-file_put_contents("contact_$dateactuelle.txt", "Civilité : $civilite \n", FILE_APPEND|LOCK_EX);
-file_put_contents("contact_$dateactuelle.txt", "Nom : $Nom\n", FILE_APPEND|LOCK_EX);
-file_put_contents("contact_$dateactuelle.txt", "Prénom : $Prenom\n", FILE_APPEND|LOCK_EX);
-file_put_contents("contact_$dateactuelle.txt", "Contact : $mail \n", FILE_APPEND|LOCK_EX);
-file_put_contents("contact_$dateactuelle.txt", "Corps du message : $Message\n", FILE_APPEND|LOCK_EX);
+$dateactuelle = date('Y-m-d-H-i-s');
+$raison = filter_input(INPUT_POST, 'Raison');
+$civilite = filter_input(INPUT_POST, 'civilite');
+$Nom = filter_input(INPUT_POST, 'Nom');
+$Prenom = filter_input(INPUT_POST, 'Prenom');
+$mail = filter_input(INPUT_POST, 'mail');
+$Message = filter_input(INPUT_POST, 'Message');
+$submit = filter_input(INPUT_POST, 'submit');
+$error=true;
+$errormsgraison="";
+$errormsgnom="";
+$errormsgprenom="";
+$errormsgcivilite="";
+$errormsgmail="";
+$errormsgmessage="";
+if (!empty($submit)) {
 
+        if (empty($raison)) {
+            $errormsgraison="Veuillez renseigner la raison";
+            $error=false;
+        }
+        if (empty($Nom)) {
+            $error=false;
+            $errormsgnom= "Veuillez renseigner votre nom";
+        }
+        if (empty($Prenom)) {
+            $error=false;
+            $errormsgprenom= "Veuillez renseigner votre prénom";
+        }
+        if (empty($civilite)) {
+            $error=false;
+            $errormsgcivilite= "Veuillez renseigner votre état civil";
+        }
+        if (empty($mail)) {
+            $error=false;
+            $errormsgmail= "Veuillez renseigner votre e-mail";
+        }
+        if (empty($Message)) {
+            $error=false;
+            $errormsgmessage= "Veuillez renseigner le corps de message";
+        }
+        if($error==true) {
+            file_put_contents("contact_$dateactuelle.txt", "Raison du contact : $raison \n", FILE_APPEND | LOCK_EX);
+            file_put_contents("contact_$dateactuelle.txt", "Civilité : $civilite \n", FILE_APPEND | LOCK_EX);
+            file_put_contents("contact_$dateactuelle.txt", "Nom : $Nom\n", FILE_APPEND | LOCK_EX);
+            file_put_contents("contact_$dateactuelle.txt", "Prénom : $Prenom\n", FILE_APPEND | LOCK_EX);
+            file_put_contents("contact_$dateactuelle.txt", "Contact : $mail \n", FILE_APPEND | LOCK_EX);
+            file_put_contents("contact_$dateactuelle.txt", "Corps du message : $Message\n", FILE_APPEND | LOCK_EX);
+        }
+
+}
 
 
 ?>
 
-    <main>
+<main>
 
-        <h2>Contact</h2>
+    <h2>Contact</h2>
 
-        <form id="formulaire" action="index.php?page=contact" method="POST">
+    <form id="formulaire" method="POST"  action="index.php?page=contact">
 
-            <section>
-                <p class="comment">Veuilllez préciser la raison de votre contact</p>
-                <input type="radio" value="Proposition d'emploi" id="Proposition d'emploi" name="Raison">
-                <label for="Proposition d'emploi">Proposition d'emploi</label>
+        <section>
+            <p class="comment">Veuilllez préciser la raison de votre contact</p> <?php echo $errormsgraison ?>
+            <input type="radio" value="Proposition d'emploi" id="Proposition d'emploi" name="Raison">
+            <label for="Proposition d'emploi">Proposition d'emploi</label>
 
-                <input type="radio" value="Demande d'informations" id="Demande d'informations" name="Raison" >
-                <label for="Demande d'informations">Demande d'informations</label>
+            <input type="radio" value="Demande d'informations" id="Demande d'informations" name="Raison">
+            <label for="Demande d'informations">Demande d'informations</label>
 
-                <input type="radio" value="Prestation" id="Prestation" name="Raison">
-                <label for="Prestation">Prestation </label>
-            </section>
+            <input type="radio" value="Prestation" id="Prestation" name="Raison">
+            <label for="Prestation">Prestation </label>
+        </section>
 
-            <section>
-                <p class="comment">Civilité</p>
-                <label for="civilité"></label>
-                <select name="civilite" id="civilité" >
-                    <option selected disabled></option>
-                    <option value="Monsieur">Monsieur</option>
-                    <option value="Madame">Madame</option>
+        <section>
+            <p class="comment">Civilité</p> <?php echo $errormsgcivilite ?>
+            <label for="civilité"></label>
+            <select name="civilite" id="civilité">
+                <option selected disabled></option>
+                <option value="Monsieur">Monsieur</option>
+                <option value="Madame">Madame</option>
 
-                </select>
-            </section>
+            </select>
+        </section>
 
-            <section>
-                <label for="Nom">Nom :</label>
-                <input type="text" id="Nom" name="Nom" placeholder="Saisissez Nom">
-            </section>
+        <section>
+            <?php echo $errormsgnom ?>
+            <label for="Nom">Nom :</label>
+            <input type="text" id="Nom" name="Nom" placeholder="Saisissez Nom">
+        </section>
 
-            <section>
-                <label for="Prenom">Prénom :</label>
-                <input type="text" id="Prénom" name="Prénom" placeholder="Saisissez Prénom" >
-            </section>
+        <section>
+            <?php echo $errormsgprenom ?>
+            <label for="Prenom">Prénom :</label>
+            <input type="text" id="Prénom" name="Prénom" placeholder="Saisissez Prénom">
+        </section>
 
-            <section>
-                <label for="Mail">e-Mail :</label>
-                <input type="email" id="Mail" name="mail" placeholder="Saisissez e-mail" >
-            </section>
+        <section>
+            <?php echo $errormsgmail ?>
+            <label for="Mail">e-Mail :</label>
+            <input type="email" id="Mail" name="mail" placeholder="Saisissez e-mail">
+        </section>
 
-            <section>
-                <label for="message">Message :</label>
-                <textarea id="message" name="Message" placeholder="Entrez votre message ici" row="20" cols="20"
-                          ></textarea>
-            </section>
+        <section>
+            <?php echo $errormsgmessage ?>
+            <label for="message">Message :</label>
+            <textarea id="message" name="Message" placeholder="Entrez votre message ici" row="20" cols="20"
+            ></textarea>
+        </section>
 
-            <section>
-                <button type="submit">Envoyer le message</button>
-            </section>
+        <section>
+            <input type="submit" name="submit" value="envoyer">
+        </section>
 
-        </form>
-    </main>
+    </form>
+</main>
